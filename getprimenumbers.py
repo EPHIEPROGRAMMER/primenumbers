9daf6575caee3a047d5290de69d59f57a4583945
MWENYEWE <ephantus.muthunga@gmail.com>
2016-10-28 17:33:10 +0300

    the input added is divided by zero

--- getprimenumbers.py
+++ getprimenumbers.py
@@ -1,4 +1,7 @@
 # A function to generate prime numbers from 0 to n with asymptotic analysis#
 def get_prime(n):
 for num in range(0,n+1):
-		if num > 1:    
+		if num > 1:
+                    for i in range(2, num);
+			if (num % i)== 0:
+				break
